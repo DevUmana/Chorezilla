9320781a042be95d2fd7ed2e090cc339342f3eef
David Umana <dumana92@gmail.com>
2024-10-05 01:19:31 -0400

    Update index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,27 @@
-import { Router } from "express";
-import authRoutes from "./auth-routes.js";
-import apiRoutes from "./api/index.js";
-import { authenticateToken } from "../middleware/auth.js";
+import { Router } from 'express';
+import authRoutes from './auth-routes.js';
+import apiRoutes from './api/index.js';
+import { authenticateToken } from '../middleware/auth.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.use("/auth", authRoutes);
-router.use(authenticateToken);
-router.use("/api", apiRoutes);
+router.use('/auth', authRoutes);
+
+router.use('/api', authenticateToken, apiRoutes);
+
+router.get('*', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../../client/dist', 'index.html'));
+  console.log('Serving index.html');
+  console.log(
+    'Path:',
+    path.resolve(__dirname, '../../../client/dist', 'index.html'),
+  );
+});
 
 export default router;
