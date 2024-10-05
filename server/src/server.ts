11e344302cd868035bec76e4fa4ae18b68ab1678
David Umana <dumana92@gmail.com>
2024-10-05 00:35:18 -0400

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,33 @@
 const forceDatabaseRefresh = false;
 
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
-import routes from "./routes/index.js";
-import { sequelize } from "./models/index.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the directory name using import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import express from 'express';
+import routes from './routes/index.js';
+import { sequelize } from './models/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static("../client/dist"));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../client/dist', 'index.html'));
+  console.log('Serving index.html');
+  console.log(
+    'Path:',
+    path.resolve(__dirname, '../../client/dist', 'index.html'),
+  );
+});
 
 app.use(express.json());
 app.use(routes);
